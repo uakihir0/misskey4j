45b7f9f92a321997ab6fb34715ce04730375e5ad
Akihiro Urushihara <a.urusihara@gmail.com>
2020-04-18 18:37:44 +0900

    bump up httpclient version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ dependencies {
 
     // J2ObjC
     compile("com.github.uakihir0", "JLogger", "1.4")
-    compile("com.github.uakihir0", "JHttpClient", "1.1.6")
+    compile("com.github.uakihir0", "JHttpClient", "1.1.8")
 
     // Library
     compile("com.google.code.gson", "gson", "2.8.6")
