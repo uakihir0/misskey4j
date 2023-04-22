0c9a0b9734abd839c6a169588b21c7cf857e14e3
Akihiro Urushiara <a.urusihara@gmail.com>
2023-04-22 14:01:33 +0900

    add publishing config.

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,3 +30,11 @@ java {
         languageVersion.set(JavaLanguageVersion.of(8))
     }
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
