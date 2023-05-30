9e43ca4a0f3541cd1569eb6c550f43a131043e84
takke <takke30@gmail.com>
2023-05-30 16:51:42 +0900

    Add channelId to `notes/create`

--- src/main/java/misskey4j/api/request/notes/NotesCreateRequest.java
+++ src/main/java/misskey4j/api/request/notes/NotesCreateRequest.java
@@ -2,8 +2,10 @@ package misskey4j.api.request.notes;
 
 import misskey4j.api.model.PollRequest;
 import misskey4j.api.model.TokenRequest;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+
 
 public class NotesCreateRequest extends TokenRequest {
 
@@ -26,6 +28,9 @@ public class NotesCreateRequest extends TokenRequest {
     private List<String> fileIds;
     private String replyId;
     private String renoteId;
+
+    @Nullable
+    private String channelId;
 
     private PollRequest poll;
 
@@ -78,6 +83,11 @@ public class NotesCreateRequest extends TokenRequest {
         return renoteId;
     }
 
+    @Nullable
+    public String getChannelId() {
+        return channelId;
+    }
+
     public PollRequest getPoll() {
         return poll;
     }
@@ -95,6 +105,7 @@ public class NotesCreateRequest extends TokenRequest {
         private List<String> fileIds;
         private String replyId;
         private String renoteId;
+        @Nullable private String channelId;
         private PollRequest poll;
 
         private NotesCreateRequestBuilder() {
@@ -160,6 +171,11 @@ public class NotesCreateRequest extends TokenRequest {
             return this;
         }
 
+        public NotesCreateRequestBuilder channelId(String channelId) {
+            this.channelId = channelId;
+            return this;
+        }
+
         public NotesCreateRequestBuilder poll(PollRequest poll) {
             this.poll = poll;
             return this;
@@ -173,6 +189,7 @@ public class NotesCreateRequest extends TokenRequest {
             notesCreateRequest.fileIds = this.fileIds;
             notesCreateRequest.cw = this.cw;
             notesCreateRequest.renoteId = this.renoteId;
+            notesCreateRequest.channelId = this.channelId;
             notesCreateRequest.noExtractHashtags = this.noExtractHashtags;
             notesCreateRequest.replyId = this.replyId;
             notesCreateRequest.visibleUserIds = this.visibleUserIds;
